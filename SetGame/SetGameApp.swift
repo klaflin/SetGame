f8e4cd142cc2437ab8eefb6b5defca3d2858edae
klaflin <kellie.laflin.26@gmail.com>
2024-03-26 12:30:01 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by KELLIE LAFLIN on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
